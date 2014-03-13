6150e715a1cdcf1635c0536c54fe905a05ecbf25
drammopo <drammopo@gmail.com>
2014-03-13 03:47:45 -0700

    Added missing file.

---
+++ lib/about_drammopo/about.rb
@@ -0,0 +1,14 @@
+class About
+	def self.first_name
+		"Dichaba"
+	end
+
+	def self.fact
+	  facts = [ 'I\'m a new fan of Ice Hockey.',
+         'My favourite team is the Chicago Blackhawks.',
+         'I like Purple.',
+         'The next language I\'m learning is Python.'
+       ]
+		facts.sample
+	end
+end
